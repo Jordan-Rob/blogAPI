4f79bc4ec8c71c171df41114026a762b424e0c5b
jordanrob709 <jordanrob709@gmail.com>
2020-05-29 23:58:05 +0300

    added authtoken app

--- blog/settings.py
+++ blog/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -83,7 +84,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ],
 }
-
 
 
 # Database
