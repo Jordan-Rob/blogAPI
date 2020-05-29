da19ef5bb4f04f9642f8e123a17b4582632a60c5
jordanrob709 <jordanrob709@gmail.com>
2020-05-29 23:46:09 +0300

    added auth settings

--- blog/settings.py
+++ blog/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [ # new
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
+    ],
 }
+
+
 
 
 # Database
