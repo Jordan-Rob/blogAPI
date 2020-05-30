9eb11b3c53c121715a8cd78e9e418e77f040193e
jordanrob709 <jordanrob709@gmail.com>
2020-05-30 21:45:52 +0300

    added django-rest-auth login/out password reset endpoints

--- blog/settings.py
+++ blog/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'rest_auth',
 ]
 
 MIDDLEWARE = [
