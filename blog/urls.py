b895fb31bc978f83430c5c95aeed55abd9bf06c4
jordanrob709 <jordanrob709@gmail.com>
2020-06-02 23:08:00 +0300

    added core api and pyyaml to create a schemafor the API

--- blog/urls.py
+++ blog/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title = 'Blog API')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,5 +25,6 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/rest-auth/', include('rest_auth.urls')),
     path('api/v1/rest-auth/registration/',
-         include('rest_auth.registration.urls'))
+         include('rest_auth.registration.urls')),
+    path('schema/', schema_view)     
 ]
